832f81a4ede3e13e787d899c11d1e0ada8c14ba4
Fernando3242 <126263030+Fernando3242@users.noreply.github.com>
2023-02-25 17:54:44 -0300

    Add files via upload

---
+++ archivos/Java.js
@@ -0,0 +1,17 @@
+
+document.getElementById("boton1").onclick = function () {
+    console.log("Cambio de texto por numero telefonico");
+    document.getElementById("N1").innerHTML = "Teléfono: 0875-432302."
+}
+document.getElementById("boton2").onclick = function () {
+    console.log("Cambio de texto por numero telefonico");
+    document.getElementById("N2").innerHTML = "Teléfono: 0875-870967."
+}
+document.getElementById("boton3").onclick = function () {
+    console.log("Cambio de texto por numeros telefonicos");
+    document.getElementById("N3").innerHTML = "Teléfono: 0875-239789, 0875-754529."
+}
+document.getElementById("boton4").onclick = function () {
+    console.log("Cambio de texto por un Email");
+    document.getElementById("N4").innerHTML = "E-mail: SideruguicaIron@gmail.com."
+}
